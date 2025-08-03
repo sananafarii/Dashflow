02848af13fdcd6dbc658125a4a67432db869599a
sana <sananafari1382@gmail.com>
2025-08-03 18:50:06 +0330

    feat:

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,7 +1,18 @@
 import { Popover, Button } from 'antd';
 import CustomAvatar from '../custom-avatar';
+import { useGetIdentity } from '@refinedev/core';
+
+
+import { User } from '@/graphql.schema.types';
+
+
 
 function CurrentUser() {
+  const {data: user } = useGetIdentity<User>();
+
+  user.
+
+
   return (
     <>
     <Popover
@@ -10,7 +21,15 @@ function CurrentUser() {
       arrow={false}
       zIndex={999}
     >
-        <CustomAvatar />
+        <CustomAvatar
+        name={user?.name}
+        src={user?.avatarUrl}
+        size={32}
+        style={{ cursor : 'pointer' }}
+         />
+
+
+         
     </Popover> 
       
     </>
