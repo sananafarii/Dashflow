640a18a8083430b30f3db570c5eebdb5f31cc885
sana <sananafari1382@gmail.com>
2025-08-05 22:03:34 +0330

    test:

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -2,6 +2,8 @@ import { Popover, Button, Typography } from 'antd';
 import CustomAvatar from '../custom-avatar';
 import { useGetIdentity } from '@refinedev/core';
 import { SettingOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import './current-user.css';
 
 const { Text } = Typography;
 
@@ -14,27 +16,36 @@ interface User {
 
 
 
-function CurrentUser() {
-  const {isOpen, setIsOpen} = useState(false)
+const CurrentUser = () => {
+  const [isOpen, setIsOpen] = useState(false)
   const {data: user } = useGetIdentity<User>();
 
 
 const content = (
-  <div style={{
+  <div className="current-user-popover-content" style={{
     display:'flex',
     flexDirection:'column',
-
+    padding: 0,
   }}>
     <Text
     strong
     style={{padding: '12px 20px'}}
-    
     >
       {user?.name}
-
     </Text>
+    <div
+    style={{
+        borderTop:'1px solid #d9d9d9',
+        padding: '4px',
+        display: 'flex',
+        flexDirection:'column',
+        gap:'4px'
+      }}
+    
+    >
+      
 
-
+    </div>
 
     <div>
       <Button
@@ -43,35 +54,36 @@ const content = (
       type="text"
       block
       onClick={() => setIsOpen(true)}
-      
       >
         Accounts Settings
-       
       </Button>
     </div>
   </div>
-
-
- 
- 
 )
 
 
   return (
+    <>
     <Popover
       placement="bottomRight"
       trigger="click"
       arrow={false}
-      zIndex={999}
+      overlayClassName="current-user-popover"
       content={content}
     >
         <CustomAvatar
         name={user?.name}
-        src={user?.avatarUrl}
-        size={32}
         style={{ cursor : 'pointer' }}
          />
     </Popover> 
+    {user && (
+      <AccountSettings
+      
+      />
+
+    )}
+    </>
+
   )
 }
 
