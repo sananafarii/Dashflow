23e089fb1c626ea5b1e11eb86e59af4ace553587
sana <sananafari1382@gmail.com>
2025-08-09 22:53:27 +0330

    fix:

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const deals-chart = () => {
+const DealsChart = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const deals-chart = () => {
   )
 }
 
-export default deals-chart
+export default DealsChart;
