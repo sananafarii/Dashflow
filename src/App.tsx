c13d4899cd74007a4f84e3416ce9cbac895c8671
sana <sananafari1382@gmail.com>
2025-08-18 21:49:02 +0330

    feat:

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,32 @@ import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import { authProvider, dataProvider, liveProvider } from "./providers";
-import { Home, ForgotPassword, Login, Register } from "./pages"
-
+import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages";
 
 import routerBindings, {
   CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
-} from "@refinedev/react-router";
+} from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
-import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
+import Create from "./pages/company/create";
+import Edit from "./pages/company/edit";
+import List from "./pages/tasks/list";
+import EditTask from "./pages/tasks/edit";
+import CreateTask from "./pages/tasks/create";
+
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
           <AntdApp>
             <DevtoolsProvider>
               <Refine
-               dataProvider={dataProvider}
-               liveProvider={liveProvider}
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 authProvider={authProvider}
@@ -36,27 +40,40 @@ function App() {
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
                   useNewQueryKeys: true,
-                  projectId: "SdvlIa-o4jCRe-DpwgVK",
+                  projectId: "WVyLEd-4karEq-tItoeC",
                   liveMode: "auto",
                 }}
               >
                 <Routes>
-                   <Route path="/register" element={<Register />} />
-                   <Route path="/login" element={<Login />} />
-                   <Route path="/forgot-password" element={<ForgotPassword />} />
-                   <Route 
-                     element={
-                       <Authenticated
-                         key="authenticated-layout"
-                         fallback={<CatchAllNavigate to="/login" />}
-                       >
-                         <Layout>
-                           <Outlet />
-                         </Layout>
-                       </Authenticated>
-                     }>
-                     <Route index element={<Home />} />
-                   </Route>
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/forgot-password" element={<ForgotPassword />} />
+                  <Route
+                    element={
+                    <Authenticated 
+                      key="authenticated-layout"
+                      fallback={<CatchAllNavigate to="/login" />}
+                    >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                    }>
+                      <Route index element={<Home />} />
+                      <Route path="/companies" >
+                        <Route index element={<CompanyList />} />
+                        <Route path="new" element={<Create />} />
+                        <Route path="edit/:id" element={<Edit />} />
+                      </Route>
+                      <Route path="/tasks" element={
+                        <List>
+                          <Outlet />
+                        </List>
+                      }>
+                        <Route path="new" element={<CreateTask />} />
+                        <Route path="edit/:id" element={<EditTask />} />
+                      </Route>
+                  </Route>
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
